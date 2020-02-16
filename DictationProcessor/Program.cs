9db8efccdd75ad5083ac61fd41d815f532f38aba
LuisTheDeveloper <Luis.Pedro.Fonseca@outlook.com>
2020-02-16 10:55:07 +0000

    new files

---
+++ DictationProcessor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Iterate through subfolders
+            foreach(var subfolder in Directory.GetDirectories("/mnt/uploads"))
+            {
+                // get metadata file
+                var metadataFilePath = Path.Combine(subfolder, "metadata.json");
+                // extract metadata file, incluing audio file info inside metadata file
+
+                // for each audio file listed in metadata:
+                // - get absolute file path
+                // - verify file checksum
+                // - generate a unique identifier
+                // - compress it
+                // - create a standalone metadata file
+
+            }
+
+        }
+    }
+}
